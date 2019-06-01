6fa467f7c409a3b4b3946f91deba0f3254bd58c2
Johannes Huber <johannes.huber@trustyou.net>
2019-06-01 10:33:26 +0200

    Fix 2-argument math functions

--- third_party/3/pyspark/sql/_typing.pyi
+++ third_party/3/pyspark/sql/_typing.pyi
@@ -1,13 +1,11 @@
-from typing import Any, List, Optional, TypeVar, Union
+from typing import Any, List, Optional, TypeVar, Union, SupportsFloat
 import datetime
 import decimal
 import pyspark.sql.column
 
 ColumnOrName = Union[pyspark.sql.column.Column, str]
+ColumnNameOrFloat = Union[ColumnOrName, SupportsFloat]
 Literal = Union[bool, int, float, str]
 DecimalLiteral = decimal.Decimal
 DateTimeLiteral = Union[datetime.datetime, datetime.date]
 LiteralType = TypeVar('LiteralType', bool, int, float, str)
-
-
-
