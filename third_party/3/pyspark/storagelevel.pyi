f9973cc60f9a51af613fe4b4922131425507dc1f
Sourabh Bajaj <sourabh@neeva.co>
2020-06-02 17:21:57 -0700

    Add StorageLevel constants

--- third_party/3/pyspark/storagelevel.pyi
+++ third_party/3/pyspark/storagelevel.pyi
@@ -18,10 +18,17 @@
 
 # Stubs for pyspark.storagelevel (Python 3.5)
 #
-
-from typing import Any
+from typing import Any, ClassVar
 
 class StorageLevel:
+    DISK_ONLY: ClassVar["StorageLevel"]
+    DISK_ONLY_2: ClassVar["StorageLevel"]
+    MEMORY_ONLY: ClassVar["StorageLevel"]
+    MEMORY_ONLY_2: ClassVar["StorageLevel"]
+    MEMORY_AND_DISK: ClassVar["StorageLevel"]
+    MEMORY_AND_DISK_2: ClassVar["StorageLevel"]
+    OFF_HEAP: ClassVar["StorageLevel"]
+
     useDisk: bool
     useMemory: bool
     useOffHeap: bool
