7f796df07da9b24202e1c447a2e8fd99b3460ceb
zero323 <mszymkiewicz@gmail.com>
2020-06-11 20:05:50 +0200

    Rename resultiterable.test  to core-resultiterable.test

--- tests/unit/testpyspark.py
+++ tests/unit/testpyspark.py
@@ -4,6 +4,7 @@ from mypy.test.testcheck import TypeCheckSuite
 
 TypeCheckSuite.files = [
     "core-context.test",
+    "core-resultiterable.test",
     "core-rdd.test",
     "ml-classification.test",
     "ml-evaluation.test",
@@ -11,7 +12,6 @@ TypeCheckSuite.files = [
     "ml-param.test",
     "ml-readable.test",
     "ml-regression.test",
-    "resultiterable.test",
     "sql-column.test",
     "sql-dataframe.test",
     "sql-pandas-typing-compatibility.test",
