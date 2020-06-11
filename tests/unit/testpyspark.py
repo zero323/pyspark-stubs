bace249097df455b579f6a6e1f4673f60342492c
zero323 <mszymkiewicz@gmail.com>
2020-06-11 21:23:01 +0200

    Rename resultiterable.test  to core-resultiterable.test

--- tests/unit/testpyspark.py
+++ tests/unit/testpyspark.py
@@ -4,15 +4,14 @@ from mypy.test.testcheck import TypeCheckSuite
 
 TypeCheckSuite.files = [
     "core-context.test",
+    "core-resultiterable.test",
     "core-rdd.test",
     "ml-evaluation.test",
     "ml-param.test",
     "ml-readable.test",
-    "resultiterable.test",
     "sql-column.test",
     "sql-dataframe.test",
     "sql-pandas-typing-compatibility.test",
     "sql-readwriter.test",
-    "core-context.test",    
     "sql-udf.test",
 ]
