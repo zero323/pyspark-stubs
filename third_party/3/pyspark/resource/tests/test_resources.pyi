663a9255878c6e39330c85f4bd5223c4e9006240
Maciej <zero323@users.noreply.github.com>
2020-09-02 23:43:27 +0200

    Drop resource.tests (#509)

--- third_party/3/pyspark/resource/tests/test_resources.pyi
+++
@@ -1,9 +0,0 @@
-import unittest
-from pyspark.resource import (
-    ExecutorResourceRequests as ExecutorResourceRequests,
-    ResourceProfileBuilder as ResourceProfileBuilder,
-    TaskResourceRequests as TaskResourceRequests,
-)
-
-class ResourceProfileTests(unittest.TestCase):
-    def test_profile_before_sc(self) -> None: ...
