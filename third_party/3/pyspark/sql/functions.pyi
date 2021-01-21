e807a1ed9fc467909d79388acdcd1807ae55b632
zero323 <mszymkiewicz@gmail.com>
2021-01-21 16:29:15 +0100

    Update to 20.8b1

--- third_party/3/pyspark/sql/functions.pyi
+++ third_party/3/pyspark/sql/functions.pyi
@@ -296,7 +296,7 @@ def pandas_udf(
 def pandas_udf(
     *,
     returnType: Union[AtomicDataTypeOrString, ArrayType],
-    functionType: PandasScalarUDFType
+    functionType: PandasScalarUDFType,
 ) -> Callable[[PandasScalarFunction], UserDefinedFunctionLike]: ...
 @overload
 def pandas_udf(
@@ -330,11 +330,11 @@ def pandas_udf(
 def pandas_udf(
     *,
     returnType: Union[AtomicDataTypeOrString, ArrayType],
-    functionType: PandasGroupedAggUDFType
+    functionType: PandasGroupedAggUDFType,
 ) -> Callable[[PandasGroupedAggFunction], UserDefinedFunctionLike]: ...
 @overload
 def pandas_udf(
     f: Union[AtomicDataTypeOrString, ArrayType],
     *,
-    functionType: PandasGroupedAggUDFType
+    functionType: PandasGroupedAggUDFType,
 ) -> Callable[[PandasGroupedAggFunction], UserDefinedFunctionLike]: ...
